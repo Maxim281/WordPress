2d9d1bc6473b3eaf782a94115e82c2f0dff0e5e8
Maksim Strelkov <maksim.strelkov29@mail.ru>
2021-05-02 21:22:08 +0400

    /

---
+++ wp-content/themes/twentytwenty/ms_build.php
@@ -0,0 +1,19 @@
+<?php /*
+* Template Name: ms_build
+* Template Post Type: post,page
+*/
+?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <?php wp_head(); ?>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no,minimum-scale=1,maximum-scale=1,user-scalable=no">
+    <link rel="icon" href="favicon.ico">
+    <title>React.js + React Router v4 + Express.js Sample App</title>
+    <link href="<?php echo esc_url ( get_template_directory_uri() ); ?>ms_build/static/css/main.57d66ede.css" rel="stylesheet">
+  </head>
+  <body>
+    <div id="root"></div>
+    <script type="text/javascript" ssrc="<?php echo esc_url ( get_template_directory_uri() ); ?>ms_build/static/js/main.7e813e83.js"></script>
+</body></html>
