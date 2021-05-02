62dee3581ccce4788a74e0a150a5e3ab6c80907b
Maksim Strelkov <maksim.strelkov29@mail.ru>
2021-05-02 21:50:09 +0400

    /

--- wp-content/themes/twentytwenty/MSbuild.php
+++ wp-content/themes/twentytwenty/MSbuild.php
@@ -1,5 +1,5 @@
 <?php /*
-* Template Name: ms_build
+* Template Name: MSbuild
 * Template Post Type: post,page
 */
 ?>
@@ -11,9 +11,9 @@
     <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no,minimum-scale=1,maximum-scale=1,user-scalable=no">
     <link rel="icon" href="favicon.ico">
     <title>React.js + React Router v4 + Express.js Sample App</title>
-    <link href="<?php echo esc_url ( get_template_directory_uri() ); ?>ms_build/static/css/main.57d66ede.css" rel="stylesheet">
+    <link href="<?php echo esc_url ( get_template_directory_uri() ); ?>MSbuild/static/css/main.57d66ede.css" rel="stylesheet">
   </head>
   <body>
     <div id="root"></div>
-    <script type="text/javascript" ssrc="<?php echo esc_url ( get_template_directory_uri() ); ?>ms_build/static/js/main.7e813e83.js"></script>
+    <script type="text/javascript" ssrc="<?php echo esc_url ( get_template_directory_uri() ); ?>MSbuild/static/js/main.7e813e83.js"></script>
 </body></html>
