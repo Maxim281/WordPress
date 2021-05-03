d912edeb8392c22d47110bfa8f48768abe6ab4e0
Maksim Strelkov <maksim.strelkov29@mail.ru>
2021-05-03 11:40:06 +0400

    /

--- wp-content/themes/twentytwenty/MSbuild.php
+++ wp-content/themes/twentytwenty/MSbuild.php
@@ -6,14 +6,15 @@
 <!doctype html>
 <html lang="en">
   <head>
-    <?php wp_head(); ?>
+  <?php wp_head(); ?>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no,minimum-scale=1,maximum-scale=1,user-scalable=no">
-    <link rel="icon" href="favicon.ico">
+    <meta name="theme-color" content="#000000"><link rel="icon" href="favicon.ico">
     <title>React.js + React Router v4 + Express.js Sample App</title>
-    <link href="<?php echo esc_url ( get_template_directory_uri() ); ?>MSbuild/static/css/main.57d66ede.css" rel="stylesheet">
+    <link href="<?php echo esc_url ( get_template_directory_uri() ); ?>/MSbuild/static/css/main.57d66ede.css" rel="stylesheet">
   </head>
   <body>
-    <div id="root"></div>
-    <script type="text/javascript" ssrc="<?php echo esc_url ( get_template_directory_uri() ); ?>MSbuild/static/js/main.7e813e83.js"></script>
-</body></html>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+      <script type="text/javascript" src="<?php echo esc_url ( get_template_directory_uri() ); ?>/MSbuild/static/js/main.7e813e83.js"></script>
+    </body>
+    </html>
